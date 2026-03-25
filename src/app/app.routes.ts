8ca8b9a33ae06871bfc4b32fd04f2d11247e86da
AeroTempest <friench08@gmail.com>
2026-03-25 19:58:03 +0800

    Reactive Form Project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-import { TemplateDemoComponent } from './template-demo/template-demo';
+import { ReactiveDemoComponent } from './reactive-demo/reactive-demo';
 
 export const routes: Routes = [
-  { path: '', component: TemplateDemoComponent },
-  { path: '**', redirectTo: '' }
+  { path: '', component: ReactiveDemoComponent }
 ];
