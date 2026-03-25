2c2d53a0d1696b38f293c311ff794e202d851f29
AeroTempest <friench08@gmail.com>
2026-03-25 19:29:31 +0800

    Final Template Driven Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TemplateDemoComponent } from './template-demo/template-demo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TemplateDemoComponent },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/template-demo/template-demo.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-template-demo',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './template-demo.html',
+  styleUrl: './template-demo.css'
+})
+export class TemplateDemoComponent {
+
+  user = {
+    username: '',
+    email: '',
+    password: '',
+    role: '',
+    gender: '',
+    status: '',
+    comments: ''
+  };
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.user);
+  }
+}
